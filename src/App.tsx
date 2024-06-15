5b3cd0c02ff65bfbbc7fe5768218a35020ed3fc0
vickyadri29 <vickyadri290501@gmail.com>
2024-06-15 14:13:25 +0800

    feat(setup): setup tailwind, husky, eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-500">test</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
