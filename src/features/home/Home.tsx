0ca21139f30e6567fdaa7028d5f75786476de327
vickyadri29 <vickyadri290501@gmail.com>
2024-06-17 21:37:04 +0800

    refact(home): remove unused code

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,28 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
-
-import { api } from "@/configs";
-import { Movie } from "@/types";
+import { useEffect } from "react";
 
 import TopRated from "./components/TopRated";
 import NowPlaying from "./components/NowPlaying";
 
 const Home = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const apiKey = import.meta.env.VITE_TMDB_API_KEY;
-
-  const handleGetListMovies = useCallback(async () => {
-    try {
-      const res = await api.get(`/list/1?api_key=${apiKey}`);
-      setMovies(res.data.items);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [apiKey]);
-
-  useEffect(() => {
-    handleGetListMovies();
-  }, [handleGetListMovies]);
-
   useEffect(() => {
     document.title = "Home page | Technical Test Seryu Cargo";
   }, []);
@@ -31,7 +12,7 @@ const Home = () => {
     <div className="space-y-10">
       <NowPlaying />
 
-      <TopRated movies={movies} />
+      <TopRated />
     </div>
   );
 };
