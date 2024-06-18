cca46647e269e8634fb3c64d51f6ed0be46dc29d
vickyadri29 <vickyadri290501@gmail.com>
2024-06-18 17:01:20 +0800

    fix: fix something issue

--- src/features/movie-detail/Cover.tsx
+++ src/features/movie-detail/Cover.tsx
@@ -1,10 +1,12 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import dayjs from "dayjs";
 
+import { useToast } from "@/components/ui/toast-provider";
 import { addToFavorite } from "@/services/favoriteService";
 import { addToWatchlist } from "@/services/watchlistService";
 import { FavoriteRequest, Movie, MovieDetails, WatchlistRequest } from "@/types";
 
+import Spin from "@/components/ui/spin";
 import iconFavoriteOutline from "@/assets/ic_favorite_outline.svg";
 import iconBookmarkOutline from "@/assets/ic_bookmark_outline.svg";
 
@@ -13,6 +15,9 @@ type Props = {
 };
 
 const Cover = ({ movie }: Props) => {
+  const { addToast } = useToast();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const baseImageUrl = import.meta.env.VITE_TBDB_BASE_IMAGE_URL;
   const backgroundCover = `${baseImageUrl}/${movie.backdrop_path}`;
   const poster = `${baseImageUrl}/${movie.poster_path}`;
@@ -29,75 +34,93 @@ const Cover = ({ movie }: Props) => {
   }, [movie.runtime]);
 
   const handleAddToFavorite = async (data: Movie) => {
+    setIsLoading(true);
+
     const payload: FavoriteRequest = {
       media_type: "movie",
-      media_id: data.id ?? 0,
+      media_id: data?.id ?? 0,
       favorite: true,
     };
+
     try {
       const res = await addToFavorite(payload);
-      console.log(res);
+      addToast(res.data.status_message, "success");
     } catch (error) {
+      addToast("Oops something went wrong!", "error");
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const handleAddToWatchlist = async (data: Movie) => {
+    setIsLoading(true);
+
     const payload: WatchlistRequest = {
       media_type: "movie",
-      media_id: data.id ?? 0,
+      media_id: data?.id ?? 0,
       watchlist: true,
     };
+
     try {
       const res = await addToWatchlist(payload);
-      console.log(res);
+      addToast(res.data.status_message, "success");
     } catch (error) {
+      addToast("Oops something went wrong!", "error");
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="relative lg:h-[400px] h-[650px]">
-      <div className="absolute w-full">
-        <div className="relative">
-          <div className="bg-black/80 w-full lg:h-[400px] h-[650px] absolute" />
-          <img src={backgroundCover} alt="backdrop_path" className="w-full lg:h-[400px] h-[650px] object-cover " />
+    <Spin spinning={isLoading}>
+      <div className="relative lg:h-[400px] h-[650px]">
+        <div className="absolute w-full">
+          <div className="relative">
+            <div className="bg-black/80 w-full lg:h-[400px] h-[650px] absolute" />
+            <img src={backgroundCover} alt="backdrop_path" className="w-full lg:h-[400px] h-[650px] object-cover " />
+          </div>
         </div>
-      </div>
-      <div className="relative max-w-[1360px] mx-auto p-5 flex md:items-center z-10 md:h-full">
-        <div className="flex md:flex-row flex-col md:items-center gap-5">
-          <img src={poster} alt="poster_path" className="md:w-[200px] w-60 md:h-[300px] h-80 object-cover rounded-md" />
-          <div className="flex flex-col gap-5">
-            {/* Title */}
-            <div className="flex flex-col gap-2">
-              <h2 className="md:text-3xl text-xl font-semibold">{movie.title}</h2>
-              <span className="md:text-sm text-xs font-roboto">
-                {dayjs(movie.release_date).format("DD/MM/YYYY")} • {renderGenres} • {renderDuration}
-              </span>
-            </div>
+        <div className="relative max-w-[1360px] mx-auto p-5 flex md:items-center z-10 md:h-full">
+          <div className="flex md:flex-row flex-col md:items-center gap-5">
+            <img
+              src={poster}
+              alt="poster_path"
+              className="md:w-[200px] w-60 md:h-[300px] h-80 object-cover rounded-md"
+            />
+            <div className="flex flex-col gap-5">
+              {/* Title */}
+              <div className="flex flex-col gap-2">
+                <h2 className="md:text-3xl text-xl font-semibold">{movie.title}</h2>
+                <span className="md:text-sm text-xs font-roboto">
+                  {dayjs(movie.release_date).format("DD/MM/YYYY")} • {renderGenres} • {renderDuration}
+                </span>
+              </div>
 
-            {/* Bookmark and Favorite Buttons */}
-            <div className="flex items-center gap-[10px]">
-              <button onClick={() => handleAddToWatchlist(movie)}>
-                <img src={iconBookmarkOutline} alt="ic_bookmark" />
-              </button>
-              <button onClick={() => handleAddToFavorite(movie)}>
-                <img src={iconFavoriteOutline} alt="ic_bookmark" />
-              </button>
-            </div>
+              {/* Bookmark and Favorite Buttons */}
+              <div className="flex items-center gap-[10px]">
+                <button onClick={() => handleAddToWatchlist(movie)}>
+                  <img src={iconBookmarkOutline} alt="ic_bookmark" />
+                </button>
+                <button onClick={() => handleAddToFavorite(movie)}>
+                  <img src={iconFavoriteOutline} alt="ic_bookmark" />
+                </button>
+              </div>
 
-            {/* Tagline and Overview */}
-            <div className="flex flex-col gap-[10px]">
-              <span className="md:text-sm text-xs italic font-roboto">{movie.tagline}</span>
-              <div className="flex flex-col gap-1">
-                <p className="md:text-sm text-xs font-bold">Overview</p>
-                <span className="md:text-sm text-xs font-roboto">{movie.overview}</span>
+              {/* Tagline and Overview */}
+              <div className="flex flex-col gap-[10px]">
+                <span className="md:text-sm text-xs italic font-roboto">{movie.tagline}</span>
+                <div className="flex flex-col gap-1">
+                  <p className="md:text-sm text-xs font-bold">Overview</p>
+                  <span className="md:text-sm text-xs font-roboto">{movie.overview}</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
